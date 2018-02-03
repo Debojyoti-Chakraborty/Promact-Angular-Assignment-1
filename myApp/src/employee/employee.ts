5aafcb84c87d00cc63587bf757c1f3c73e061dd1
Debojyoti Chakraborty <debojyoti@promactinfo.com>
2018-02-03 17:20:49 +0530

    Added Interface IEmployee

---
+++ myApp/src/employee/employee.ts
@@ -0,0 +1,13 @@
+export interface IEmployee{
+    firstName:string;
+    lastName:string;
+    email:string;
+    contactNumber:number;
+    address:string;
+    userName:string;
+    password:string;
+    gender:string;
+    qualification:string;
+    experience:string;
+    languages:string[];
+}
