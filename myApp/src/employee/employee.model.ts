48d85029829f06859dbeca791b5a3de5d346bd43
Debojyoti Chakraborty <debojyoti@promactinfo.com>
2018-02-05 12:57:41 +0530

    Added Employee model class

---
+++ myApp/src/employee/employee.model.ts
@@ -0,0 +1,19 @@
+export class Employee{
+    constructor(
+    public firstName:string,
+    public lastName:string,
+    public email:string,
+    public contactNumber:string,
+    public address:string,
+    public userName:string,
+    public password:string,
+    public gender:string,
+    public qualification:string,
+    public experience:string,
+    public languages: string[],
+            
+    )
+    {
+        
+    }
+}
