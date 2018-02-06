4e0309ee2c7712e290206acaea42c75699ec5ed2
Debojyoti Chakraborty <debojyoti@promactinfo.com>
2018-02-06 17:00:31 +0530

    Fixed Typo

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -4,5 +4,5 @@ import { Component } from '@angular/core';
     templateUrl: 'app.component.html'
 })
 export class AppComponent{
-    pageTitle: string = 'Employee Magaement Application';
+    pageTitle: string = 'Employee Management Application';
 }
