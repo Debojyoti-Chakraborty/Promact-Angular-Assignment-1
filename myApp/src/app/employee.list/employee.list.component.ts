8b07296abf42f0e5191fa6e7d17dff8599c62c54
Debojyoti Chakraborty <debojyoti@promactinfo.com>
2018-02-06 13:10:14 +0530

    Added employee.list

---
+++ myApp/src/app/employee.list/employee.list.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+import { Employee } from '../employee/employee.model';
+
+@Component({
+    templateUrl:'./employee.list.html'
+})
+export class EmpListComponent{}
