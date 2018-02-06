bc9c54ee2313bf1f97acd5df5c89da4f1420f95a
Debojyoti Chakraborty <debojyoti@promactinfo.com>
2018-02-06 17:04:31 +0530

    Included sharing service to fetch objects

--- myApp/src/app/employee.list/employee.list.component.ts
+++ myApp/src/app/employee.list/employee.list.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee/employee.model';
-
+import { SharingService } from '../app.sharing.service';
+import { Router }         from '@angular/router';
 @Component({
     templateUrl:'./employee.list.html'
 })
-export class EmpListComponent{}
+export class EmpListComponent implements OnInit{
+
+    
+      data:any=[];
+
+    constructor(private router:Router,
+        private sharingService:SharingService){}
+
+    ngOnInit(){
+        this.data=this.sharingService.getData());
+        console.log(this.data);
+    }
+    delrec(emp):void{
+        this.sharingService.deldata(emp);
+        this.data=this.sharingService.getData());
+    }
+}
