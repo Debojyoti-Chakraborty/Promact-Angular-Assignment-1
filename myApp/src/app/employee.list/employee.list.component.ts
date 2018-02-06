607951cefe151ff727a716ce4a67726516554995
Debojyoti Chakraborty <debojyoti@promactinfo.com>
2018-02-06 18:51:17 +0530

    Solved an issue where confirmation was being asked for editing as well

--- myApp/src/app/employee.list/employee.list.component.ts
+++ myApp/src/app/employee.list/employee.list.component.ts
@@ -17,15 +17,24 @@ export class EmpListComponent implements OnInit{
         this.data=this.sharingService.getData();
         console.log(this.data);
     }
-    delrec(emp):void{
+    delrec(emp,i:number):void{
+        if(i==0){
         if (window.confirm("Are You Sure You Want to delete This Record?")) {
             this.sharingService.deldata(emp);
+            this.data=this.sharingService.getData();
+        }
+        }
+        else if(i==1){
+             this.sharingService.deldata(emp);
             this.data=this.sharingService.getData();
         }
     }
     editrec(emp,url:string):void{
         this.sharingService.setObj(emp);
-        this.delrec(emp);
+        this.delrec(emp,1);
+        this.router.navigate([url]);
+    }
+    redir(url:string):void{
         this.router.navigate([url]);
     }
 }
